9ca418941c80af17d7224264246e85e6d2f1f263
haochuchu <987875638@qq.com>
2017-08-22 10:58:29 +0800

    8.22

---
+++ src/component/Yezhu_accept.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+
+class YezhuAccept extends Component {
+  render() {
+    return (
+    	  <div>
+    	  	88888888888
+        </div>
+    );
+  }
+}
+
+export default YezhuAccept;
