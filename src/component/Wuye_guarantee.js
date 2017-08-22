b03b3332164eb4d56055824a5b98984b4516e852
guohaojing <13663677607@qq.com>
2017-08-22 18:39:57 +0800

    8.22   物业邮包
    Signed-off-by: guohaojing <13663677607@qq.com>

--- src/component/Wuye_guarantee.js
+++ src/component/Wuye_guarantee.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import './../css/wuye_guarantee.css';
+import wuyealready from './wuye_already'
 
 import {
   BrowserRouter as Router,
@@ -8,11 +10,48 @@ import {
 
 
 class WuyeGuarantee extends Component {
+  tip=function(){
+    window.history.go(-1)
+  }
   render() {
     return (
-    	  <div>
-    	  	31464534
+      <Router>
+    	  <div className="guarantee">
+            <div className="go_back">
+                    <span>保修服务</span>
+                </div>
+    	  	  <div className="guarantee_top">
+                <p><Link exact to="/wuye/index/guarantee">未处理</Link></p>
+                <p><Link to="/wuye/index/steward">已处理</Link></p>
+            </div>
+            <Route path="/wuye/index/guarantee" render={()=>(
+                <div className="guarantee_bottom">
+                    <ul className="repari">
+                        <li>
+                            <div className="repari_top">
+                                <p>感应灯坏了，一直都不亮，请师傅尽快安排维修，要不然没办法走路。</p>
+                            </div>
+                            <div className="repari_bottom">
+                                <div className="book">
+                                    <p className="porto"></p>
+                                    <div className="porto_con">
+                                        <p>大圣</p>
+                                        <p>大圣花园小区</p>
+                                    </div>
+                                </div>
+                                <div className="tener">标记为处理</div>
+                            </div>
+                        </li>
+                    </ul>
+                     <div className="tip" onClick={this.tip}>
+                        <p><img src="../../images/tip.png" alt=""/></p>
+                    </div>
+                </div>
+              )} />
+            <Route path="/wuye/index/steward" component={wuyealready} />
+            
         </div>
+      </Router>
     );
   }
 }
--- src/component/Wuyeindex.js
+++ src/component/Wuyeindex.js
@@ -4,6 +4,11 @@ import Wuyezhuzhe from './Wuyezhuzhe'
 import Wuyesteward from './Wuyesteward'
 import Wuyeshequ from './Wuyeshequ'
 import Wuyeme from './Wuyeme'
+import  WuyeParcel from './Wuye_parcel';
+import  WuyeGuarantee from './Wuye_guarantee';
+import  WuyeNotice from './Wuye_notice';
+import  WuyeOrganize from './Wuye_organize';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -20,6 +25,10 @@ class Wuyeindex extends Component {
             <Route path="/wuye/index/steward" component={Wuyesteward} />
             <Route path="/wuye/index/shequ" component={Wuyeshequ} />
             <Route path="/wuye/index/me" component={Wuyeme} />
+            <Route path="/wuye/index/parcel" component={WuyeParcel}/>
+            <Route path="/wuye/index/notice" component={WuyeNotice}/>
+            <Route path="/wuye/index/organize" component={WuyeOrganize}/>
+            <Route path="/wuye/index/guarantee" component={WuyeGuarantee}/>
             <div className="admin_con">
                 <Link to="/wuye/index">住这儿</Link>
                 <Link to="/wuye/index/steward">管家</Link>
