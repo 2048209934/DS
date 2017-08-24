f9fabbb222aa3b627a606bd98b09efd369a80191
haochuchu <987875638@qq.com>
2017-08-24 11:02:49 +0800

    8.24

--- src/action/hcc.js
+++ src/action/hcc.js
@@ -1,2 +1,22 @@
 import config from './../config';
 import $ from 'jquery';
+
+
+export function package(village,address){
+	return dispatch=>{
+		return $.ajax({
+			type:"post",
+			url:config.url+"/mail/yzmail",
+			data:{
+				village:village,
+				address:address
+			},
+			success:function(data){
+				
+			},
+			error:function(){
+				alert("0.0")
+			}
+		});	
+	}
+}
