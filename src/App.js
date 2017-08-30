698e847371d729e062026b7ee588d3c5d4f85fad
guohaojing <13663677607@qq.com>
2017-08-30 19:38:32 +0800

    8.30

--- src/App.js
+++ src/App.js
@@ -15,11 +15,25 @@ class App extends Component {
       <Router>
         <div className="App">
             <Route exact path='/' render={() => (
-            	<div className="app">
-            		<Link to="/boss">我是BOSS</Link>
-	            	<Link to="/wuye">我是物业</Link>
-	            	<Link to="/yezhu">我是业主</Link>   
-            	</div>
+            	<div className="app_con">
+                  <div className="dl_top">
+                      <p> LIVE   HERE </p>
+                      <p>mahatma</p>
+                  </div>
+                 {/* <div className="app">
+                    <Link to="/boss">我是BOSS</Link>
+                    <Link to="/wuye">我是物业</Link>
+                    <Link to="/yezhu">我是业主</Link>   
+                  </div>*/}
+                  <div className="circle">
+                        <div className="circle_con">
+                            <img src="images/app.png" alt=""/>
+                        </div>
+                        <Link to="/boss" className="circle_one"><img src="images/boss.png" alt=""/><p>boss</p></Link>
+                        <Link to="/yezhu" className="circle_two"><img src="images/yezhu.png" alt=""/><p>owner</p></Link>
+                        <Link to="/wuye" className="circle_three"><img src="images/wuye.png" alt=""/><p>property</p></Link>
+                  </div>
+              </div>
             )} />             
             <Route path="/boss" component={Boss} />
             <Route path="/wuye" component={Wuye} />
--- src/component/Bossindex.js
+++ src/component/Bossindex.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 import './../css/bossindex.css';
+
 import Bosszhuzhe from './Bosszhuzhe';
 import Bosssteward from './Bosssteward';
 import Bossshequ from './Bossshequ';
 import Bossme from './Bossme';
+
+import $ from 'jquery';
+import OldMenuLink from './Navs';
+
 import {
   BrowserRouter as Router,
   Route,
@@ -12,6 +17,7 @@ import {
 
 
 class Bossindex extends Component {
+  
   render() {
     return (
     	<Router>
@@ -20,15 +26,21 @@ class Bossindex extends Component {
     	  	<Route path="/boss/index/steward" component={Bosssteward} />
     	  	<Route path="/boss/index/shequ" component={Bossshequ} />
     	  	<Route path="/boss/index/me" component={Bossme} />
-	        <h2 id="a">111</h2>
-    	  	<Link to="/boss/index">住这儿</Link>
+	         <div className="boss_con">
+                <OldMenuLink to={'/boss/index'} activeOnlyWhenExact={true} label={'住这儿'} img1={'../images/account_eight.png'} img2={'../images/account_server.png'}></OldMenuLink>
+                <OldMenuLink to={'/boss/index/steward'} label={'管家'} img1={'../images/account_five.png'} img2={'../images/account_six.png'}></OldMenuLink>
+                <OldMenuLink to={'/boss/index/shequ'} label={'社区'} img1={'../images/account_four.png'} img2={'../images/account_three.png'}></OldMenuLink>
+            </div>
+    	  	{/*<Link to="/boss/index">住这儿</Link>
     	  	<Link to="/boss/index/steward">管家</Link>
     	  	<Link to="/boss/index/shequ">社区</Link>
     	  	<Link to="/boss/index/me">我</Link>
+        */}
         </div>
     	 </Router>   
     );
   }
 }
+
 
 export default Bossindex;
